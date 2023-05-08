05ee64d551f2f65dff4e9b16a6c448fd1746000c
Gabriel Scopono <gabriel.scopono@gmail.com>
2023-05-08 00:22:46 -0300

    commit inicial

---
+++ src/01-intro/aula01.py
@@ -0,0 +1,11 @@
+""" aula 01 """
+
+print('ola mundo')
+
+def somar(numero_01, numero_02):
+    """ somar - soma dois numeros """
+    return numero_01 + numero_02
+
+SOMA = somar(25, 20)
+
+print(SOMA)
