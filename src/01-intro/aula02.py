fc57f20b2b6a85cea9d1d93d7824e10e5c7a2f27
Gabriel Scopono <gabriel.scopono@gmail.com>
2023-05-08 17:33:27 -0300

    Adição da aula 2 no repositório

---
+++ src/01-intro/aula02.py
@@ -0,0 +1,32 @@
+"""Aula 02 - Identificadores"""
+
+# Keywords - Palavras reservadas, como true, false, none, and, import, lambda etc
+
+# Identificadores - Servem para identificar elementos no algoritmo, como nomes de variáveis, funções ,classes
+# Case sensitive
+# Devem iniciar com uma letra ou _
+# não pode ter espaços em branco
+
+nome = 'Maria'
+ídade = 30
+
+Nome = 'João'
+nome_completo = 'Maria da Silva'
+
+# Boas práticas (clean code)
+c = 10
+contador = 10
+
+s = 10 + 20
+soma = 10 + 20
+
+# Constantes
+PI = 3.14
+
+idade = 18
+MAIORIDADE = 18
+
+if idade >= MAIORIDADE:
+    print('Maior de idade')
+else:
+    print('Menor de idade')
