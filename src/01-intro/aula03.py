84530c1a018a07a6a2fb1cdcbcb830f7c1423ace
Gabriel Scopono <gabriel.scopono@gmail.com>
2023-05-08 17:33:59 -0300

    Adição da aula 3 no repositório

---
+++ src/01-intro/aula03.py
@@ -0,0 +1,20 @@
+"""Aula 03 - Comentários"""
+
+# Comentário de única linah
+
+'''
+Comentário
+de
+várias
+linhas
+'''
+
+# n1 - 10
+# n2 = 30       (ctrl + / → Comenta as linhas selecionadas)
+# n3 = 78
+
+# Um bom código geralmente não precisa de comentários, visto que a intenção de um código já vai estar esclarecida pelos identificadores
+# Se uma função for uma grande, como 30 linhas, podemos subdividir essa função em várias outras funções diferentes que realizam partes
+# da tarefa
+# Não é boa prática um código com muitos comentários, sendo utilizados mais para partes mais complexas ou para entender o porquê de uma
+# decisão tomada no código.
